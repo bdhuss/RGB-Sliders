cec11950b79fc59e6b2677b24f8aaa777ba104b7
Bryan Huss <bryan.huss@gmail.com>
2020-02-09 18:52:09 -0500

    RGB sliders active. Added personal touch.

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSliderBar: UISlider!
+    @IBOutlet weak var greenSliderBar: UISlider!
+    @IBOutlet weak var blueSliderBar: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        // Set app background to white
+        self.view.backgroundColor = UIColor.white
+        
+        // Set all sliders to 1.0 for a white background
+        redSliderBar.value = 1.0
+        greenSliderBar.value = 1.0
+        blueSliderBar.value = 1.0
     }
 
 
+    @IBAction func sliderChanged(_ sender: Any) {
+        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(redSliderBar.value), green: CGFloat(greenSliderBar.value), blue: CGFloat(blueSliderBar.value), alpha: 1.0)
+    }
 }
 
