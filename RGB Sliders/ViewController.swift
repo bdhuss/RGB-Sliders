d05bbbc7816d1628a2e8bb0cdb1164e4b6b7400c
Bryan Huss <bryan.huss@gmail.com>
2020-02-09 21:27:24 -0500

    removed all UI elements from storyboard

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -10,26 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var redSliderBar: UISlider!
-    @IBOutlet weak var greenSliderBar: UISlider!
-    @IBOutlet weak var blueSliderBar: UISlider!
+    let redLabel = UILabel()
+    let greenLabel = UILabel()
+    let blueLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    
         // Set app background to white
         self.view.backgroundColor = UIColor.white
         
-        // Set all sliders to 1.0 for a white background
-        redSliderBar.value = 1.0
-        greenSliderBar.value = 1.0
-        blueSliderBar.value = 1.0
+        // set up labels
+        redLabel.text = "Red"
+        redLabel.textColor = .black
+        self.view.addSubview(redLabel)
+        
     }
 
-
-    @IBAction func sliderChanged(_ sender: Any) {
-        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(redSliderBar.value), green: CGFloat(greenSliderBar.value), blue: CGFloat(blueSliderBar.value), alpha: 1.0)
-    }
 }
-
